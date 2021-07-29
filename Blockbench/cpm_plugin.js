0ca5b1f4f7aa2619544aaf07ead3341c23540354
tom5454 <tom5454a@gmail.com>
2021-07-29 21:34:58 +0200

    Blockbench plugin 0.0.3
    Tex Scale and Scale now imports correctly.
    Added support for per-face UV cubes

--- Blockbench/cpm_plugin.js
+++ Blockbench/cpm_plugin.js
@@ -5,59 +5,143 @@
         author: 'tom5454',
         description: 'Customizable Player Models Project (.cpmproject) support for Blockbench.',
         icon: 'icon-player',
-        version: '0.0.2',
+        version: '0.0.3',
         variant: 'both',
         onload() {
-        	var VANILLA_VALUES = {
-        			head: {
-        				pos: {x: 0.0, y: 0.0, z: 0.0},
-        				offset: {x: -4.0, y: -8.0, z: -4.0},
-        				size: {x: 8.0, y: 8.0, z: 8.0},
-        				uv: {u: 0, v: 0},
-        			},
-        			body: {
-        				pos: {x: 0.0, y: 0.0, z: 0.0},
-        				offset: {x: -4.0, y: 0.0, z: -2.0},
-        				size: {x: 8.0, y: 12.0, z: 4.0},
-        				uv: {u: 16, v: 16},
-        			},
-        			left_arm: {
-        				pos: {x: 5.0, y: 2.0, z: 0.0},
-        				offset: {x: -1.0, y: -2.0, z: -2.0},
-        				size: {x: 4.0, y: 12.0, z: 4.0},
-        				uv: {u: 32, v: 48},
-        			},
-        			right_arm: {
-        				pos: {x: -5.0, y: 2.0, z: 0.0},
-        				offset: {x: -3.0, y: -2.0, z: -2.0},
-        				size: {x: 4.0, y: 12.0, z: 4.0},
-        				uv: {u: 40, v: 16},
-        			},
-        			left_arm: {
-        				pos: {x: 5.0, y: 2.0, z: 0.0},
-        				offset: {x: -1.0, y: -2.0, z: -2.0},
-        				size: {x: 3.0, y: 12.0, z: 4.0},
-        				uv: {u: 32, v: 48},
-        			},
-        			right_arm: {
-        				pos: {x: -5.0, y: 2.0, z: 0.0},
-        				offset: {x: -2.0, y: -2.0, z: -2.0},
-        				size: {x: 3.0, y: 12.0, z: 4.0},
-        				uv: {u: 40, v: 16},
-        			},
-        			left_leg: {
-        				pos: {x: 1.9, y: 12.0, z: 0.0},
-        				offset: {x: -2.0, y: 0.0, z: -2.0},
-        				size: {x: 4.0, y: 12.0, z: 4.0},
-        				uv: {u: 16, v: 48},
-        			},
-        			right_leg: {
-        				pos: {x: -1.9, y: 12.0, z: 0.0},
-        				offset: {x: -2.0, y: 0.0, z: -2.0},
-        				size: {x: 4.0, y: 12.0, z: 4.0},
-        				uv: {u: 0, v: 16},
-        			},
-        		};
+			var VANILLA_VALUES = {
+				head: {
+					pos: { x: 0.0, y: 0.0, z: 0.0 },
+					offset: { x: -4.0, y: -8.0, z: -4.0 },
+					size: { x: 8.0, y: 8.0, z: 8.0 },
+					uv: { u: 0, v: 0 },
+				},
+				body: {
+					pos: { x: 0.0, y: 0.0, z: 0.0 },
+					offset: { x: -4.0, y: 0.0, z: -2.0 },
+					size: { x: 8.0, y: 12.0, z: 4.0 },
+					uv: { u: 16, v: 16 },
+				},
+				left_arm: {
+					pos: { x: 5.0, y: 2.0, z: 0.0 },
+					offset: { x: -1.0, y: -2.0, z: -2.0 },
+					size: { x: 4.0, y: 12.0, z: 4.0 },
+					uv: { u: 32, v: 48 },
+				},
+				right_arm: {
+					pos: { x: -5.0, y: 2.0, z: 0.0 },
+					offset: { x: -3.0, y: -2.0, z: -2.0 },
+					size: { x: 4.0, y: 12.0, z: 4.0 },
+					uv: { u: 40, v: 16 },
+				},
+				left_arm: {
+					pos: { x: 5.0, y: 2.0, z: 0.0 },
+					offset: { x: -1.0, y: -2.0, z: -2.0 },
+					size: { x: 3.0, y: 12.0, z: 4.0 },
+					uv: { u: 32, v: 48 },
+				},
+				right_arm: {
+					pos: { x: -5.0, y: 2.0, z: 0.0 },
+					offset: { x: -2.0, y: -2.0, z: -2.0 },
+					size: { x: 3.0, y: 12.0, z: 4.0 },
+					uv: { u: 40, v: 16 },
+				},
+				left_leg: {
+					pos: { x: 1.9, y: 12.0, z: 0.0 },
+					offset: { x: -2.0, y: 0.0, z: -2.0 },
+					size: { x: 4.0, y: 12.0, z: 4.0 },
+					uv: { u: 16, v: 48 },
+				},
+				right_leg: {
+					pos: { x: -1.9, y: 12.0, z: 0.0 },
+					offset: { x: -2.0, y: 0.0, z: -2.0 },
+					size: { x: 4.0, y: 12.0, z: 4.0 },
+					uv: { u: 0, v: 16 },
+				},
+				cape: {
+					pos: { x: 0.0, y: 0.0, z: 0.0 },
+					offset: { x: -5.0, y: 0.0, z: -1.0 },
+					size: { x: 10.0, y: 16.0, z: 1.0 },
+					uv: { u: 0, v: 0 },
+					custom: true,
+				},
+				elytra_left: {
+					pos: { x: 0.0, y: 0.0, z: 0.0 },
+					offset: { x: -10.0, y: 0.0, z: 0.0 },
+					size: { x: 10.0, y: 20.0, z: 2.0 },
+					uv: { u: 22, v: 0 },
+					custom: true,
+				},
+				elytra_right: {
+					pos: { x: 0.0, y: 0.0, z: 0.0 },
+					offset: { x: 0.0, y: 0.0, z: 0.0 },
+					size: { x: 10.0, y: 20.0, z: 2.0 },
+					uv: { u: 22, v: 0 },
+					custom: true,
+				},
+				armor_helmet: {
+					pos: { x: 0.0, y: 0.0, z: 0.0 },
+					offset: { x: -4.0, y: -8.0, z: -4.0 },
+					size: { x: 8.0, y: 8.0, z: 8.0 },
+					uv: { u: 0, v: 0 },
+					custom: true,
+				},
+				armor_body: {
+					pos: { x: 0.0, y: 0.0, z: 0.0 },
+					offset: { x: -4.0, y: 0.0, z: -2.0 },
+					size: { x: 8.0, y: 12.0, z: 4.0 },
+					uv: { u: 16, v: 16 },
+					custom: true,
+				},
+				armor_left_arm: {
+					pos: { x: 5.0, y: 2.0, z: 0.0 },
+					offset: { x: -1.0, y: -2.0, z: -2.0 },
+					size: { x: 4.0, y: 12.0, z: 4.0 },
+					uv: { u: 40, v: 16 },
+					custom: true,
+				},
+				armor_right_arm: {
+					pos: { x: -5.0, y: 2.0, z: 0.0 },
+					offset: { x: -3.0, y: -2.0, z: -2.0 },
+					size: { x: 4.0, y: 12.0, z: 4.0 },
+					uv: { u: 40, v: 16 },
+					custom: true,
+				},
+				armor_leggings_body: {
+					pos: { x: 0.0, y: 0.0, z: 0.0 },
+					offset: { x: -4.0, y: 0.0, z: -2.0 },
+					size: { x: 8.0, y: 12.0, z: 4.0 },
+					uv: { u: 16, v: 16 },
+					custom: true,
+				},
+				armor_left_leg: {
+					pos: { x: 1.9, y: 12.0, z: 0.0 },
+					offset: { x: -2.0, y: 0.0, z: -2.0 },
+					size: { x: 4.0, y: 12.0, z: 4.0 },
+					uv: { u: 0, v: 16 },
+					custom: true,
+				},
+				armor_right_leg: {
+					pos: { x: -1.9, y: 12.0, z: 0.0 },
+					offset: { x: -2.0, y: 0.0, z: -2.0 },
+					size: { x: 4.0, y: 12.0, z: 4.0 },
+					uv: { u: 0, v: 16 },
+					custom: true,
+				},
+				armor_left_foot: {
+					pos: { x: 1.9, y: 12.0, z: 0.0 },
+					offset: { x: -2.0, y: 0.0, z: -2.0 },
+					size: { x: 4.0, y: 12.0, z: 4.0 },
+					uv: { u: 0, v: 16 },
+					custom: true,
+				},
+				armor_right_foot: {
+					pos: { x: -1.9, y: 12.0, z: 0.0 },
+					offset: { x: -2.0, y: 0.0, z: -2.0 },
+					size: { x: 4.0, y: 12.0, z: 4.0 },
+					uv: { u: 0, v: 16 },
+					custom: true,
+				},
+			};
 			var NAME_MAP = {
 				Head: "head",
 				Body: "body",
@@ -82,6 +166,7 @@
     			compile(options) {
 					let all_groups = getAllGroups();
 					let loose_cubes = [];
+					let box_uv = Project.box_uv;
 					Cube.all.forEach(cube => {
 						if (cube.parent == 'root') loose_cubes.push(cube)
 					})
@@ -136,6 +221,41 @@
 					p.skinSize.y = Project.texture_height;
 					p.elements = [];
 					let modelTree = {};
+					function convert(elem, cube) {
+						if(box_uv) {
+							elem.u = cube.uv_offset[0];
+							elem.v = cube.uv_offset[1];
+							elem.mirror = cube.mirror_uv;
+						} else {
+							elem.faceUV = {};
+							for (var faceName of Object.keys(cube.faces)) {
+								let face = cube.faces[faceName];
+								
+								if(faceName == "east")faceName = "west";
+								else if(faceName == "west")faceName = "east";
+								
+								if(face.uv_size[0] != 0 && face.uv_size[1] != 0) {
+									let f = {};
+									elem.faceUV[faceName] = f;
+									f.sx = face.uv[0];
+									f.sy = face.uv[1];
+									f.ex = face.uv[2];
+									f.ey = face.uv[3];
+									if(faceName == "up" || faceName == "down")
+										f.rot = ((face.rotation + 180) % 360).toString();
+									else
+										f.rot = face.rotation.toString();
+									f.autoUV = cube.autouv ? true : false;
+								}
+							}
+							elem.u = 0;
+							elem.v = 0;
+							elem.mirror = false;
+						}
+						elem.mcScale = cube.inflate;
+						elem.offset = {x: group.origin[0] - cube.to[0], y: -cube.from[1] - cube.size(1) + group.origin[1], z: cube.from[2] - group.origin[2]};
+						elem.size = {x: cube.size(0, true), y: cube.size(1, true), z: cube.size(2, true)};
+					}
 					for (var group of all_groups) {
 						if ((group instanceof Group === false && !group.is_catch_bone) || !group.export) continue;
 						if (group.parent instanceof Group) continue;
@@ -170,13 +290,8 @@
 							elem.textureSize = 1;
 							elem.texture = true;
 							elem.show = true;
-							elem.u = cube.uv_offset[0];
-							elem.v = cube.uv_offset[1];
-							elem.mcScale = cube.inflate;
-							elem.mirror = cube.mirror_uv;
+							convert(elem, cube);
 							elem.pos = {x: 0, y: 0, z: 0};
-							elem.offset = {x: group.origin[0] - cube.to[0], y: -cube.from[1] - cube.size(1) + group.origin[1], z: cube.from[2] - group.origin[2]};
-							elem.size = {x: cube.size(0, true), y: cube.size(1, true), z: cube.size(2, true)};
 							elem.color = "0";
 						}
 					}
@@ -204,12 +319,7 @@
 						for (var cube of group.children) {
 							if (cube instanceof Cube === false || !cube.export || (!cube.rotation.allEqual(0) && !group.is_rotation_subgroup)) continue;
 							e = jQuery.extend(true, {}, elem);
-							e.u = cube.uv_offset[0];
-							e.v = cube.uv_offset[1];
-							e.mcScale = cube.inflate;
-							e.mirror = cube.mirror_uv;
-							e.offset = {x: group.origin[0] - cube.to[0], y: -cube.from[1] - cube.size(1) + group.origin[1], z: cube.from[2] - group.origin[2]};
-							e.size = {x: cube.size(0, true), y: cube.size(1, true), z: cube.size(2, true)};
+							convert(e, cube);
 							e.show = cube.visibility;
 							e.name = cube.name;
 							modelTree[group.parent.uuid].push(e);
@@ -277,6 +387,49 @@
     						Project.texture_width = p.skinSize.x;
     						Project.texture_height = p.skinSize.y;
     						Project.box_uv = true;
+		
+							function ceil(val) {
+								return Math.ceil(val - 0.01);
+							}
+							
+							function floor(val) {
+								return Math.floor(val + 0.01);
+							}
+		
+							function boxToPFUV(cube, texSc = 1, scX = 1, scY = 1, scZ = 1) {
+								var dx = ceil(cube.to[0] - cube.from[0]) * texSc / (!scX ? 1 : scX);
+								var dy = ceil(cube.to[1] - cube.from[1]) * texSc / (!scY ? 1 : scY);
+								var dz = ceil(cube.to[2] - cube.from[2]) * texSc / (!scZ ? 1 : scZ);
+								var u = floor(cube.uv_offset[0] * texSc);
+								var v = floor(cube.uv_offset[1] * texSc);
+								var f4 = u;
+								var f5 = u + dz;
+								var f6 = u + dz + dx;
+								var f7 = u + dz + dx + dx;
+								var f8 = u + dz + dx + dz;
+								var f9 = u + dz + dx + dz + dx;
+								var f10 = v;
+								var f11 = v + dz;
+								var f12 = v + dz + dy;
+								cube.faces.up.uv = [f6, f11, f5, f10];
+								cube.faces.down.uv = [f7, f10, f6, f11];
+								cube.faces.east.uv = [f4, f11, f5, f12];
+								cube.faces.north.uv = [f5, f11, f6, f12];
+								cube.faces.west.uv = [f6, f11, f8, f12];
+								cube.faces.south.uv = [f8, f11, f9, f12];
+								if (cube.mirror_uv) {
+									for (var faceName of Object.keys(cube.faces)) {
+										let face = cube.faces[faceName];
+										var t = face.uv[0];
+										face.uv[0] = face.uv[2];
+										face.uv[2] = t;
+									}
+									var uv = cube.faces.east.uv;
+									cube.faces.east.uv = cube.faces.west.uv;
+									cube.faces.west.uv = uv;
+								}
+								cube.autouv = 0;
+							}
     						
     						function loadChildren(data, parent) {
     							for(var i = 0;i<data.length;i++) {
@@ -300,8 +453,8 @@
 	    							var cube = new Cube({
 	    								name: e.name,
 										from: [
-											gr.origin[0] - e.offset.x - e.size.x,
-											gr.origin[1] - e.offset.y - e.size.y,
+											gr.origin[0] - e.offset.x - e.size.x * e.scale.x,
+											gr.origin[1] - e.offset.y - e.size.y * e.scale.y,
 											gr.origin[2] + e.offset.z
 										],
 										inflate: e.mcScale,
@@ -310,11 +463,106 @@
 									});
 									cube.extend({
 										to: [
-											cube.from[0] + Math.floor(e.size.x),
-											cube.from[1] + Math.floor(e.size.y),
-											cube.from[2] + Math.floor(e.size.z),
+											cube.from[0] + floor(e.size.x * e.scale.x),
+											cube.from[1] + floor(e.size.y * e.scale.y),
+											cube.from[2] + floor(e.size.z * e.scale.z),
 										]
 									});
+									if(e.faceUV || e.textureSize > 1 || e.singleTex || e.scale.x != 1 || e.scale.y != 1 || e.scale.z != 1) {
+										if(Project.box_uv) {
+											Project.box_uv = false;
+											Cube.all.forEach(c => boxToPFUV(c));
+										}
+										cube.autouv = 0;
+										if(e.faceUV) {
+											for (var faceName of Object.keys(cube.faces)) {
+												let face = cube.faces[faceName];
+												
+												if(faceName == "east")faceName = "west";
+												else if(faceName == "west")faceName = "east";
+												
+												if(e.faceUV[faceName]) {
+													var ef = e.faceUV[faceName];
+													face.uv = [ef.sx, ef.sy, ef.ex, ef.ey];
+													if(faceName == "up" || faceName == "down")
+														face.rotation = ((ef.rot + 180) % 360);
+													else
+														face.rotation = ef.rot;
+												} else {
+													face.uv = [0, 0, 0, 0];
+												}
+											}
+										} else if(e.singleTex) {
+											if(e.mcScale == 0 && (e.pos.x == 0 || e.pos.y == 0 || e.pos.z == 0)) {
+												var texU = e.u * e.textureSize;
+												var texV = e.v * e.textureSize;
+												if(e.pos.x == 0) {
+													var tu = texU + ceil(e.size.z * e.textureSize);
+													var tv = texV + ceil(e.size.y * e.textureSize);
+													for (var faceName of Object.keys(cube.faces)) {
+														let face = cube.faces[faceName];
+														if(faceName == "west") {
+															face.uv = [texU, texV, tu, tv];
+														} else if(faceName == "east") {
+															face.uv = [tu, texV, texU, tv];
+														} else {
+															face.uv = [0, 0, 0, 0];
+														}
+													}
+												} else if(e.pos.y == 0) {
+													var tu = texU + ceil(e.size.x * e.textureSize);
+													var tv = texV + ceil(e.size.z * e.textureSize);
+													for (var faceName of Object.keys(cube.faces)) {
+														let face = cube.faces[faceName];
+														if(faceName == "up") {
+															face.uv = [tu, texV, texU, tv];
+														} else if(faceName == "down") {
+															face.uv = [texU, texV, tu, tv];
+														} else {
+															face.uv = [0, 0, 0, 0];
+														}
+													}
+												} else if(e.pos.z == 0) {
+													var tu = texU + ceil(e.size.x * e.textureSize);
+													var tv = texV + ceil(e.size.y * e.textureSize);
+													for (var faceName of Object.keys(cube.faces)) {
+														let face = cube.faces[faceName];
+														if(faceName == "north") {
+															face.uv = [texU, texV, tu, tv];
+														} else if(faceName == "south") {
+															face.uv = [tu, texV, texU, tv];
+														} else {
+															face.uv = [0, 0, 0, 0];
+														}
+													}
+												}
+											} else {
+												var size = Math.max(e.pos.x, e.pos.y, e.pos.z);
+												for (var faceName of Object.keys(cube.faces)) {
+													let face = cube.faces[faceName];
+													face.uv[0] = e.u * e.textureSize;
+													face.uv[1] = e.v * e.textureSize;
+													face.uv[2] = (e.u + size) * e.textureSize;
+													face.uv[3] = (e.v + size) * e.textureSize;
+												}
+											}
+											if(e.mirror) {
+												for (var faceName of Object.keys(cube.faces)) {
+													let face = cube.faces[faceName];
+													var t = face.uv[0];
+													face.uv[0] = face.uv[2];
+													face.uv[2] = t;
+												}
+												var uv = cube.faces.east.uv;
+												cube.faces.east.uv = cube.faces.west.uv;
+												cube.faces.west.uv = uv;
+											}
+										} else {
+											boxToPFUV(cube, e.textureSize, e.scale.x, e.scale.y, e.scale.z);
+										}
+									} else if(!Project.box_uv) {
+										boxToPFUV(cube);
+									}
 									cube.addTo(gr).init();
 									if (e.children) {
 	    								loadChildren(e.children, gr);
@@ -344,13 +592,16 @@
 	    							});
 									cube.extend({
 										to: [
-											cube.from[0] + Math.floor(vals.size.x),
-											cube.from[1] + Math.floor(vals.size.y),
-											cube.from[2] + Math.floor(vals.size.z),
+											cube.from[0] + floor(vals.size.x),
+											cube.from[1] + floor(vals.size.y),
+											cube.from[2] + floor(vals.size.z),
 										]
 									});
 									cube.visibility = data.show;
     								cube.addTo(gr).init();
+									if(!Project.box_uv) {
+										boxToPFUV(cube);
+									}
 									
     								if(data.children) {
     									loadChildren(data.children, gr);
@@ -385,7 +636,6 @@
     					csname = files[0].name.replace(".cpmproject", "").replace(/\s+/g, "_").toLowerCase();
     					Project.name = csname;
     					Project.geometry_name = csname;
-    					//textures[0].name = csname;
     				})
     			}
     		})
