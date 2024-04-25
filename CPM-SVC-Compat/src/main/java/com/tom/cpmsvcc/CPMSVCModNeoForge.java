4480f5f4843ba099ca6c707bfcc361cc1c1901b4
tom5454 <tom5454a@gmail.com>
2024-04-25 21:17:36 +0200

    update CPMSVC and CPMOSC to NeoForge 1.20.5

--- CPM-SVC-Compat/src/main/java/com/tom/cpmsvcc/CPMSVCModNeoForge.java
+++ CPM-SVC-Compat/src/main/java/com/tom/cpmsvcc/CPMSVCModNeoForge.java
@@ -6,14 +6,9 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.InterModComms;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.InterModEnqueueEvent;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(CPMSVCC.MOD_ID)
 public class CPMSVCModNeoForge {
-
-	public CPMSVCModNeoForge() {
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::sendIMC);
-	}
 
 	public CPMSVCModNeoForge(IEventBus bus) {
 		bus.addListener(this::sendIMC);
