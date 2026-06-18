9b53c2fdf15d0952919a5a258b8f5a8b14168c10
Apehum <apehum@proton.me>
2026-06-18 14:54:39 +0800

    Revert "use pv's AudioUtil for audio level calculations"
    This reverts commit 1aeafb0fc2c3be3a5f73b9598fcf536d8752d578.

--- CPM-PV-Compat/src/shared/java/com/tom/cpmpvc/CPMPVC.java
+++ CPM-PV-Compat/src/shared/java/com/tom/cpmpvc/CPMPVC.java
@@ -11,7 +11,6 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
 import com.tom.cpm.shared.MinecraftClientAccess;
-import su.plo.voice.api.util.AudioUtil;
 
 public class CPMPVC {
 	public static final String MOD_ID = "cpmpvc";
@@ -35,10 +34,62 @@ public class CPMPVC {
 	}
 
 	private static float calcVoiceLevel(short[] data) {
-		return (float) AudioUtil.audioLevelToDoubleRange(AudioUtil.calculateHighestAudioLevel(data));
+		return (float) dbToPerc(getHighestAudioLevel(data));
 	}
 
 	public static boolean isMuted(UUID uuid) {
 		return CPMAddon.INSTANCE.isMuted(uuid);
+	}
+
+	/**
+	 * Calculates the audio level of a signal with specific samples.
+	 *
+	 * @param samples the samples of the signal to calculate the audio level of
+	 * @param offset  the offset in samples in which the samples start
+	 * @param length  the length in bytes of the signal in samples starting at offset
+	 * @return the audio level of the specified signal in db
+	 */
+	private static double calculateAudioLevel(short[] samples, int offset, int length) {
+		double rms = 0D; // root mean square (RMS) amplitude
+
+		for (int i = offset; i < length; i++) {
+			double sample = (double) samples[i] / (double) Short.MAX_VALUE;
+			rms += sample * sample;
+		}
+
+		int sampleCount = length / 2;
+
+		rms = (sampleCount == 0) ? 0 : Math.sqrt(rms / sampleCount);
+
+		double db;
+
+		if (rms > 0D) {
+			db = Math.min(Math.max(20D * Math.log10(rms), -127D), 0D);
+		} else {
+			db = -127D;
+		}
+
+		return db;
+	}
+
+	/**
+	 * Calculates the highest audio level in packs of 100
+	 *
+	 * @param samples the audio samples
+	 * @return the audio level in db
+	 */
+	private static double getHighestAudioLevel(short[] samples) {
+		double highest = -127D;
+		for (int i = 0; i < samples.length; i += 100) {
+			double level = calculateAudioLevel(samples, i, Math.min(i + 100, samples.length));
+			if (level > highest) {
+				highest = level;
+			}
+		}
+		return highest;
+	}
+
+	private static double dbToPerc(double db) {
+		return (db + 127D) / 127D;
 	}
 }
